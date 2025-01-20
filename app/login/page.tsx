9da6d60fce5bea264794c8372a1fcd233d09bd99
snowypy <git@snowyjs.lol>
2025-01-20 18:55:34 +0000

    eslint stuff

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,6 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Card, CardContent, CardHeader } from "@/components/ui/card"
-import {Footer} from "@/components/footer";
 
 export default function LoginPage() {
     return (
