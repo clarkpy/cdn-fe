99b2deed203695dc5d6f848b523aa09a7b24cfd8
snowypy <git@snowyjs.lol>
2025-01-20 18:51:21 +0000

    create footer.tsx

---
+++ components/footer.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export function Footer() {
+    return (
+        <footer className="bg-transparent text-center py-3">
+            <div className="px-4 text-sm container mx-auto">
+                <p>
+                    Made with <span className="text-primary">❤️</span> by {" "}
+                    <span className="relative">
+                        <Link href="https://github.com/snowypy">Cigan</Link>
+                    </span>
+                </p>
+            </div>
+        </footer>
+    )
+}
